38b12fc17283dc46e0a49b8518c785ce72cb7ee3
Almar Klein <almar.klein@gmail.com>
2015-02-19 14:38:35 +0100

    Add docs on zoof.ui

--- conf.py
+++ conf.py
@@ -21,7 +21,8 @@ import sphinx_bootstrap_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('scripts'))
 
 # -- General configuration ------------------------------------------------
 
@@ -32,7 +33,7 @@ import sphinx_bootstrap_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'sphinx.ext.autodoc', 'uiexample'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
